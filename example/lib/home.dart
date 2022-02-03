207bc5d034ed0aa6dd1223d1d40083a509603570
Ulrich Diedrichsen <uli@ud-softworks.dev>
2022-02-03 13:56:42 +0100

    First version with rendering json object

--- example/lib/home.dart
+++ example/lib/home.dart
@@ -1,8 +1,22 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:json_explorer/json_explorer.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Map<String, dynamic>> loadAssetJson(String assetFilename) async {
+    String data = await rootBundle.loadString(assetFilename);
+
+    return json.decode(data) as Map<String, dynamic>;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +26,18 @@ class MyHomePage extends StatelessWidget {
           'JsonExplorer',
         ),
       ),
-      body: const JsonExplorer(
-        {
-          'test': 'test',
+      body: FutureBuilder<Map<String, dynamic>>(
+        future: loadAssetJson('assets/json/petstore.swagger.json'),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return JsonExplorer(
+              snapshot.data!,
+              filter: const ['post', 'put', 'delete', 'security', 'responses'],
+              show: const ['paths'],
+            );
+          }
+
+          return const Center(child: CircularProgressIndicator());
         },
       ),
     );
